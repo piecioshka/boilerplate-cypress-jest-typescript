477dfcd769cb2a525b98e66b291a567cfa4ac90a
Piotr Kowalski <piecioshka@gmail.com>
2021-12-19 22:55:40 +0100

    Set indent to 2 spaces

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -1,12 +1,12 @@
-describe('General', function () {
-    it('should display a logo', function () {
-        cy.visit('/blog/');
-        cy.get('#me img');
-    });
+describe("General", function () {
+  it("should display a logo", function () {
+    cy.visit("/blog/");
+    cy.get("#me img");
+  });
 
-    it('should redirect to homepage after click on logo', () => {
-        cy.visit('/blog/');
-        cy.get('#me img').click();
-        cy.url().should('equal', 'https://piecioshka.pl/');
-    });
+  it("should redirect to homepage after click on logo", () => {
+    cy.visit("/blog/");
+    cy.get("#me img").click();
+    cy.url().should("equal", "https://piecioshka.pl/");
+  });
 });
